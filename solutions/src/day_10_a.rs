06edd8a866fe7271006c5659bede9a1745f5b78b
dmulher <dominic.mulherin@gmail.com>
2023-12-08 14:43:43 +1000

    Pre-generating all days

---
+++ solutions/src/day_10_a.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+extern crate test;
+
+pub fn main(contents: String) -> u32 {
+  0
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use test::Bencher;
+  use utils::read_file_to_string;
+
+  const DAY: u8 = 10;
+  const PART: utils::Part = utils::Part::A;
+
+  #[test]
+  fn test_day_10_a() {
+    const EXAMPLE_ANSWER: u32 = 0;
+    const ANSWER: Option<u32> = None;
+    match utils::run_method::<u32>(&main, DAY, PART, (EXAMPLE_ANSWER, ANSWER)) {
+      Err(message) => panic!("{}", message),
+      Ok(val) if ANSWER.is_none() => println!("Answer for day {DAY}-{} = {val}", PART.lower_name()),
+      _ => (),
+    }
+  }
+
+  #[bench]
+  fn bench_day_10_a(b: &mut Bencher) {
+    let input = read_file_to_string(utils::get_file_name(DAY, None).as_str());
+    b.iter(|| main(input.clone()));
+  }
+}
---
+++ solutions/src/day_12_a.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+extern crate test;
+
+pub fn main(contents: String) -> u32 {
+  0
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use test::Bencher;
+  use utils::read_file_to_string;
+
+  const DAY: u8 = 12;
+  const PART: utils::Part = utils::Part::A;
+
+  #[test]
+  fn test_day_12_a() {
+    const EXAMPLE_ANSWER: u32 = 0;
+    const ANSWER: Option<u32> = None;
+    match utils::run_method::<u32>(&main, DAY, PART, (EXAMPLE_ANSWER, ANSWER)) {
+      Err(message) => panic!("{}", message),
+      Ok(val) if ANSWER.is_none() => println!("Answer for day {DAY}-{} = {val}", PART.lower_name()),
+      _ => (),
+    }
+  }
+
+  #[bench]
+  fn bench_day_12_a(b: &mut Bencher) {
+    let input = read_file_to_string(utils::get_file_name(DAY, None).as_str());
+    b.iter(|| main(input.clone()));
+  }
+}
