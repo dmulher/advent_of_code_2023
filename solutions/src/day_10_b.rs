9a761332beff5fe1b9fc6b6935a34868b458a1a1
dmulher <dominic.mulherin@gmail.com>
2023-12-11 08:50:47 +1000

    Slightly improved

--- solutions/src/day_10_b.rs
+++ solutions/src/day_10_b.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque, HashSet};
+use std::collections::{HashMap/*, VecDeque*/, HashSet};
 
 extern crate test;
 
@@ -14,16 +14,16 @@ enum Direction {
   West,
 }
 
-impl Direction {
-  fn reverse(self) -> Direction {
-    match self {
-      Direction::East => Direction::West,
-      Direction::West => Direction::East,
-      Direction::North => Direction::South,
-      Direction::South => Direction::North,
-    }
-  }
-}
+// impl Direction {
+//   fn reverse(self) -> Direction {
+//     match self {
+//       Direction::East => Direction::West,
+//       Direction::West => Direction::East,
+//       Direction::North => Direction::South,
+//       Direction::South => Direction::North,
+//     }
+//   }
+// }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum Pipe {
@@ -33,6 +33,38 @@ enum Pipe {
   Vertical,
   Corner(Direction, Direction)
 }
+
+impl From<char> for Pipe {
+  fn from(c: char) -> Self {
+    match c {
+      '|' => Pipe::Vertical,
+      '-' => Pipe::Horizontal,
+      'L' => Pipe::Corner(Direction::North, Direction::East),
+      'J' => Pipe::Corner(Direction::North, Direction::West),
+      '7' => Pipe::Corner(Direction::South, Direction::West),
+      'F' => Pipe::Corner(Direction::South, Direction::East),
+      '.' => Pipe::Ground,
+      'S' => Pipe::Starting,
+      _ => panic!("Error"),
+    }
+  }
+}
+
+// impl Pipe {
+//   fn to_char(self) -> char {
+//     match self {
+//       Pipe::Vertical => '|',
+//       Pipe::Horizontal => '-',
+//       Pipe::Corner(Direction::North, Direction::East) => 'L',
+//       Pipe::Corner(Direction::North, Direction::West) => 'J',
+//       Pipe::Corner(Direction::South, Direction::West) => '7',
+//       Pipe::Corner(Direction::South, Direction::East) => 'F',
+//       Pipe::Ground => '.',
+//       Pipe::Starting => 'S',
+//       _ => panic!("Error"),
+//     }
+//   }
+// }
 
 fn find_all_captured_pieces(contents: String) -> u32 {
   let (start_idx, mut map) = build_map(&contents);
@@ -51,25 +83,26 @@ fn find_all_captured_pieces(contents: String) -> u32 {
   map.insert(start_idx, real_starting_pipe);
   let path = follow_path(start_idx, entry_move_direction, &map, height, width);
 
-  let mut visited: HashSet<(usize, usize)> = HashSet::new();
+  return just_iterate_again(&map, width, height, &path);
+  // let mut visited: HashSet<(usize, usize)> = HashSet::new();
 
-  // Get all non-path internal nodes
-  let mut captures: VecDeque<((usize, usize), Direction)> = get_inside_boundary(&map, &mut visited, &path, height, width);
+  // // Get all non-path internal nodes
+  // let mut captures: VecDeque<((usize, usize), Direction)> = get_inside_boundary(&map, &mut visited, &path, height, width);
 
-  let mut total: u32 = 0;
-  // Iterate through the internal nodes and spread out
-  while let Some((inside, dir)) = captures.pop_front() {
-    let inside_pipe = map.get(&inside);
-    if let Some(_) = inside_pipe {
-      total += 1;
-    }
+  // let mut total: u32 = 0;
+  // // Iterate through the internal nodes and spread out
+  // while let Some((inside, dir)) = captures.pop_front() {
+  //   let inside_pipe = map.get(&inside);
+  //   if let Some(_) = inside_pipe {
+  //     total += 1;
+  //   }
 
-    for (pos, dir) in get_all_neighbouring_dir(inside, dir, &map, height, width, &mut visited) {
-      captures.push_back((pos, dir));
-    }
-  }
+  //   for (pos, dir) in get_all_neighbouring_dir(inside, dir, &map, height, width, &mut visited) {
+  //     captures.push_back((pos, dir));
+  //   }
+  // }
 
-  total
+  // total
 }
 
 fn build_map(contents: &String) -> ((usize, usize), HashMap<(usize, usize), Pipe>) {
@@ -82,7 +115,7 @@ fn build_map(contents: &String) -> ((usize, usize), HashMap<(usize, usize), Pipe
         if c == 'S' {
           start_idx = (i, j);
         }
-        ((i, j), char_to_pipe(c))
+        ((i, j), Pipe::from(c))
       }).collect::<Vec<((usize, usize), Pipe)>>()
     })
     .collect::<HashMap<(usize, usize), Pipe>>();
@@ -113,41 +146,32 @@ fn get_starting_pipe(start_idx: (usize, usize), map: &HashMap<(usize, usize), Pi
   if starting_north && starting_south { Pipe::Vertical } else if starting_east && starting_west { Pipe::Horizontal } else {Pipe::Corner(starting_dirs[0], starting_dirs[1])}
 }
 
-fn get_inside_boundary(map: &HashMap<(usize, usize), Pipe>, visited: &mut HashSet<(usize, usize)>, path: &HashSet<(usize, usize)>, height: usize, width: usize) -> VecDeque<((usize, usize), Direction)> {
-  let mut captures: VecDeque<((usize, usize), Direction)> = VecDeque::new();
-  let entry_point = get_inside(&map, height, width, visited, &path);
-  let entry_move_direction = Direction::West;
-  let entry_look_direction = Direction::South;
-
-  // Get the outer-most part
-  let mut next_cell = Some((entry_point, entry_move_direction, entry_look_direction));
-  // Iterate through the path again, now knowing what the inside is
-  while let Some((pos, move_dir, look_dir)) = next_cell {
-    // Check in look direction, move along path
-    if !visited.insert(pos) {
-      break;
-    }
-    for looked_capture in look_around(pos, look_dir, &map, height, width, visited, &path) {
-      captures.push_back(looked_capture);
-    }
-    let curr_pipe = map.get(&pos);
-    let next_dir = match curr_pipe {
-      Some(Pipe::Horizontal | Pipe::Vertical) => move_dir,
-      Some(Pipe::Corner(ns, es)) => follow_corner(ns, es, &move_dir),
-      _ => panic!("Something went wrong"),
-    };
-    let next_look = match curr_pipe {
-      Some(Pipe::Horizontal | Pipe::Vertical) => look_dir,
-      Some(Pipe::Corner(ns, es)) => keep_look_orientation_around_corner(ns, es, &look_dir),
-      _ => panic!("Something went wrong"),
-    };
-    if let Some(next_pos) = apply_dir_to_pos(pos, next_dir, height, width) {
-      next_cell = Some((next_pos, next_dir, next_look));
-    } else {
-      next_cell = None;
+fn just_iterate_again(map: &HashMap<(usize, usize), Pipe>, width: usize, height: usize, path: &HashSet<(usize, usize)>) -> u32 {
+  let mut total: u16 = 0;
+  for j in 0..height {
+    let mut primed: Option<Direction> = None;
+    let mut crossed: u8 = 0;
+    for i in 0..width {
+      let pos = (i, j);
+      let in_path = path.contains(&pos);
+      if !in_path && crossed % 2 == 1 {
+        total += 1;
+      } else if in_path {
+        match (map.get(&pos), primed) {
+          (Some(Pipe::Vertical), _) => crossed += 1,
+          (Some(Pipe::Corner(prime_dir, Direction::East)), None) => primed = Some(*prime_dir),
+          (Some(Pipe::Corner(Direction::North, Direction::West)), Some(Direction::South)) |
+          (Some(Pipe::Corner(Direction::South, Direction::West)), Some(Direction::North)) => {
+            primed = None;
+            crossed += 1;
+          },
+          (Some(Pipe::Corner(_, _)), Some(_)) => primed = None,
+          (_, _) => {}
+        }
+      }
     }
   }
-  captures
+  total as u32
 }
 
 fn follow_path(start_idx: (usize, usize), entry_move_direction: Direction, map: &HashMap<(usize, usize), Pipe>, height: usize, width: usize) -> HashSet<(usize, usize)> {
@@ -187,77 +211,6 @@ fn apply_dir_to_pos(pos: (usize, usize), dir: Direction, height: usize, width: u
   }
 }
 
-fn get_inside(map: &HashMap<(usize, usize), Pipe>, height: usize, width: usize, visited: &mut HashSet<(usize, usize)>, path: &HashSet<(usize, usize)>) -> (usize, usize) {
-  for j in 0..height {
-    for i in 0..width {
-      let map_val = map.get(&(i, j));
-      if let Some(Pipe::Ground) = map.get(&(i, j)) {
-        visited.insert((i, j));
-        continue;
-      } else if !path.contains(&(i, j)) {
-        visited.insert((i, j));
-        continue;
-      } else if let Some(&Pipe::Corner(Direction::South, Direction::East)) = map_val {
-        return (i, j);
-      }
-    }
-  }
-  panic!("Something went wrong");
-}
-
-fn look_around(pos: (usize, usize), looking: Direction, map: &HashMap<(usize, usize), Pipe>, height: usize, width: usize, visited: &mut HashSet<(usize, usize)>, path: &HashSet<(usize, usize)>) -> Vec<((usize, usize), Direction)> {
-  let pipe_type = map.get(&pos);
-  let mut looked_spaces = vec![];
-  if let Some(Pipe::Corner(ns, ew)) = pipe_type {
-    if is_looking_outside_corner(ns, ew, &looking) {
-
-      let corner_look = keep_look_orientation_around_corner(ns, ew, &looking);
-      if let Some(corner_look_pos) = apply_dir_to_pos(pos, corner_look, height, width) {
-        if !visited.contains(&corner_look_pos) && !path.contains(&corner_look_pos) {
-          visited.insert(corner_look_pos);
-          looked_spaces.push((corner_look_pos, corner_look));
-        }
-      }
-    } else {
-      return looked_spaces;
-    }
-  }
-  if let Some(look_pos) = apply_dir_to_pos(pos, looking, height, width) {
-    if !visited.contains(&look_pos) && !path.contains(&look_pos) {
-      visited.insert(look_pos);
-      looked_spaces.push((look_pos, looking));
-    }
-  }
-  looked_spaces
-}
-
-fn is_looking_outside_corner(north_south: &Direction, east_west: &Direction, look_dir: &Direction) -> bool {
-  look_dir == &north_south.reverse() || look_dir == &east_west.reverse()
-}
-
-fn keep_look_orientation_around_corner(north_south: &Direction, east_west: &Direction, look_dir: &Direction) -> Direction {
-  // Ugly, but verbose
-  match (north_south, east_west, look_dir) {
-    (Direction::North, Direction::West, Direction::West) => Direction::North,
-    (Direction::North, Direction::West, Direction::East) => Direction::South,
-    (Direction::North, Direction::West, Direction::North) => Direction::West,
-    (Direction::North, Direction::West, Direction::South) => Direction::East,
-    (Direction::South, Direction::West, Direction::West) => Direction::South,
-    (Direction::South, Direction::West, Direction::East) => Direction::North,
-    (Direction::South, Direction::West, Direction::North) => Direction::East,
-    (Direction::South, Direction::West, Direction::South) => Direction::West,
-    (Direction::North, Direction::East, Direction::West) => Direction::South,
-    (Direction::North, Direction::East, Direction::East) => Direction::North,
-    (Direction::North, Direction::East, Direction::North) => Direction::East,
-    (Direction::North, Direction::East, Direction::South) => Direction::West,
-    (Direction::South, Direction::East, Direction::West) => Direction::North,
-    (Direction::South, Direction::East, Direction::East) => Direction::South,
-    (Direction::South, Direction::East, Direction::North) => Direction::West,
-    (Direction::South, Direction::East, Direction::South) => Direction::East,
-    _ => panic!("Something has gone terribly wrong"),
-  }
-}
-
 fn follow_corner(north_south: &Direction, east_west: &Direction, travel_dir: &Direction) -> Direction {
   match (north_south, east_west, travel_dir) {
     (ns, _, Direction::East | Direction::West) => *ns,
@@ -265,56 +218,150 @@ fn follow_corner(north_south: &Direction, east_west: &Direction, travel_dir: &Di
   }
 }
 
-fn get_all_neighbouring_dir(pos: (usize, usize), dir: Direction, map: &HashMap<(usize, usize), Pipe>, height: usize, width: usize, visited: &mut HashSet<(usize, usize)>) -> Vec<((usize, usize), Direction)> {
-  let mut dirs: Vec<((usize, usize), Direction)> = Vec::new();
-  if dir != Direction::East {
-    if let Some(neighbouring_dir) = get_next_pos(pos, Direction::West, map, height, width, visited) {
-      dirs.push(neighbouring_dir);
-    }
-  }
-  if dir != Direction::West {
-    if let Some(neighbouring_dir) = get_next_pos(pos, Direction::East, map, height, width, visited) {
-      dirs.push(neighbouring_dir);
-    }
-  }
-  if dir != Direction::North {
-    if let Some(neighbouring_dir) = get_next_pos(pos, Direction::South, map, height, width, visited) {
-      dirs.push(neighbouring_dir);
-    }
-  }
-  if dir != Direction::South {
-    if let Some(neighbouring_dir) = get_next_pos(pos, Direction::North, map, height, width, visited) {
-      dirs.push(neighbouring_dir);
-    }
-  }
-  dirs
-}
+// fn get_inside_boundary(map: &HashMap<(usize, usize), Pipe>, visited: &mut HashSet<(usize, usize)>, path: &HashSet<(usize, usize)>, height: usize, width: usize) -> VecDeque<((usize, usize), Direction)> {
+//   let mut captures: VecDeque<((usize, usize), Direction)> = VecDeque::new();
+//   let entry_point = get_inside(&map, height, width, visited, &path);
+//   let entry_move_direction = Direction::West;
+//   let entry_look_direction = Direction::South;
 
-fn get_next_pos(pos: (usize, usize), moving: Direction, map: &HashMap<(usize, usize), Pipe>, height: usize, width: usize, visited: &mut HashSet<(usize, usize)>) -> Option<((usize, usize), Direction)> {
-  let next_pos = apply_dir_to_pos(pos, moving, height, width)?;
-  if !visited.insert(next_pos) {
-    return None;
-  }
-  if let Some(_) = map.get(&next_pos) {
-    Some((next_pos, moving))
-  } else {
-    None
-  }
-}
+//   // Get the outer-most part
+//   let mut next_cell = Some((entry_point, entry_move_direction, entry_look_direction));
+//   // Iterate through the path again, now knowing what the inside is
+//   while let Some((pos, move_dir, look_dir)) = next_cell {
+//     // Check in look direction, move along path
+//     if !visited.insert(pos) {
+//       break;
+//     }
+//     for looked_capture in look_around(pos, look_dir, &map, height, width, visited, &path) {
+//       captures.push_back(looked_capture);
+//     }
+//     let curr_pipe = map.get(&pos);
+//     let next_dir = match curr_pipe {
+//       Some(Pipe::Horizontal | Pipe::Vertical) => move_dir,
+//       Some(Pipe::Corner(ns, es)) => follow_corner(ns, es, &move_dir),
+//       _ => panic!("Something went wrong"),
+//     };
+//     let next_look = match curr_pipe {
+//       Some(Pipe::Horizontal | Pipe::Vertical) => look_dir,
+//       Some(Pipe::Corner(ns, es)) => keep_look_orientation_around_corner(ns, es, &look_dir),
+//       _ => panic!("Something went wrong"),
+//     };
+//     if let Some(next_pos) = apply_dir_to_pos(pos, next_dir, height, width) {
+//       next_cell = Some((next_pos, next_dir, next_look));
+//     } else {
+//       next_cell = None;
+//     }
+//   }
+//   captures
+// }
 
-fn char_to_pipe(c: char) -> Pipe {
-  match c {
-    '|' => Pipe::Vertical,
-    '-' => Pipe::Horizontal,
-    'L' => Pipe::Corner(Direction::North, Direction::East),
-    'J' => Pipe::Corner(Direction::North, Direction::West),
-    '7' => Pipe::Corner(Direction::South, Direction::West),
-    'F' => Pipe::Corner(Direction::South, Direction::East),
-    '.' => Pipe::Ground,
-    'S' => Pipe::Starting,
-    _ => panic!("Error"),
-  }
-}
+// fn get_inside(map: &HashMap<(usize, usize), Pipe>, height: usize, width: usize, visited: &mut HashSet<(usize, usize)>, path: &HashSet<(usize, usize)>) -> (usize, usize) {
+//   for j in 0..height {
+//     for i in 0..width {
+//       let map_val = map.get(&(i, j));
+//       if let Some(Pipe::Ground) = map.get(&(i, j)) {
+//         visited.insert((i, j));
+//         continue;
+//       } else if !path.contains(&(i, j)) {
+//         visited.insert((i, j));
+//         continue;
+//       } else if let Some(&Pipe::Corner(Direction::South, Direction::East)) = map_val {
+//         return (i, j);
+//       }
+//     }
+//   }
+//   panic!("Something went wrong");
+// }
+
+// fn look_around(pos: (usize, usize), looking: Direction, map: &HashMap<(usize, usize), Pipe>, height: usize, width: usize, visited: &mut HashSet<(usize, usize)>, path: &HashSet<(usize, usize)>) -> Vec<((usize, usize), Direction)> {
+//   let pipe_type = map.get(&pos);
+//   let mut looked_spaces = vec![];
+//   if let Some(Pipe::Corner(ns, ew)) = pipe_type {
+//     if is_looking_outside_corner(ns, ew, &looking) {
+
+//       let corner_look = keep_look_orientation_around_corner(ns, ew, &looking);
+//       if let Some(corner_look_pos) = apply_dir_to_pos(pos, corner_look, height, width) {
+//         if !visited.contains(&corner_look_pos) && !path.contains(&corner_look_pos) {
+//           visited.insert(corner_look_pos);
+//           looked_spaces.push((corner_look_pos, corner_look));
+//         }
+//       }
+//     } else {
+//       return looked_spaces;
+//     }
+//   }
+//   if let Some(look_pos) = apply_dir_to_pos(pos, looking, height, width) {
+//     if !visited.contains(&look_pos) && !path.contains(&look_pos) {
+//       visited.insert(look_pos);
+//       looked_spaces.push((look_pos, looking));
+//     }
+//   }
+//   looked_spaces
+// }
+
+// fn is_looking_outside_corner(north_south: &Direction, east_west: &Direction, look_dir: &Direction) -> bool {
+//   look_dir == &north_south.reverse() || look_dir == &east_west.reverse()
+// }
+
+// fn keep_look_orientation_around_corner(north_south: &Direction, east_west: &Direction, look_dir: &Direction) -> Direction {
+//   // Ugly, but verbose
+//   match (north_south, east_west, look_dir) {
+//     (Direction::North, Direction::West, Direction::West) => Direction::North,
+//     (Direction::North, Direction::West, Direction::East) => Direction::South,
+//     (Direction::North, Direction::West, Direction::North) => Direction::West,
+//     (Direction::North, Direction::West, Direction::South) => Direction::East,
+//     (Direction::South, Direction::West, Direction::West) => Direction::South,
+//     (Direction::South, Direction::West, Direction::East) => Direction::North,
+//     (Direction::South, Direction::West, Direction::North) => Direction::East,
+//     (Direction::South, Direction::West, Direction::South) => Direction::West,
+//     (Direction::North, Direction::East, Direction::West) => Direction::South,
+//     (Direction::North, Direction::East, Direction::East) => Direction::North,
+//     (Direction::North, Direction::East, Direction::North) => Direction::East,
+//     (Direction::North, Direction::East, Direction::South) => Direction::West,
+//     (Direction::South, Direction::East, Direction::West) => Direction::North,
+//     (Direction::South, Direction::East, Direction::East) => Direction::South,
+//     (Direction::South, Direction::East, Direction::North) => Direction::West,
+//     (Direction::South, Direction::East, Direction::South) => Direction::East,
+//     _ => panic!("Something has gone terribly wrong"),
+//   }
+// }
+
+// fn get_all_neighbouring_dir(pos: (usize, usize), dir: Direction, map: &HashMap<(usize, usize), Pipe>, height: usize, width: usize, visited: &mut HashSet<(usize, usize)>) -> Vec<((usize, usize), Direction)> {
+//   let mut dirs: Vec<((usize, usize), Direction)> = Vec::new();
+//   if dir != Direction::East {
+//     if let Some(neighbouring_dir) = get_next_pos(pos, Direction::West, map, height, width, visited) {
+//       dirs.push(neighbouring_dir);
+//     }
+//   }
+//   if dir != Direction::West {
+//     if let Some(neighbouring_dir) = get_next_pos(pos, Direction::East, map, height, width, visited) {
+//       dirs.push(neighbouring_dir);
+//     }
+//   }
+//   if dir != Direction::North {
+//     if let Some(neighbouring_dir) = get_next_pos(pos, Direction::South, map, height, width, visited) {
+//       dirs.push(neighbouring_dir);
+//     }
+//   }
+//   if dir != Direction::South {
+//     if let Some(neighbouring_dir) = get_next_pos(pos, Direction::North, map, height, width, visited) {
+//       dirs.push(neighbouring_dir);
+//     }
+//   }
+//   dirs
+// }
+
+// fn get_next_pos(pos: (usize, usize), moving: Direction, map: &HashMap<(usize, usize), Pipe>, height: usize, width: usize, visited: &mut HashSet<(usize, usize)>) -> Option<((usize, usize), Direction)> {
+//   let next_pos = apply_dir_to_pos(pos, moving, height, width)?;
+//   if !visited.insert(next_pos) {
+//     return None;
+//   }
+//   if let Some(_) = map.get(&next_pos) {
+//     Some((next_pos, moving))
+//   } else {
+//     None
+//   }
+// }
 
 #[cfg(test)]
 mod tests {
