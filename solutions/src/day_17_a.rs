d5f12069ace0f7085d6b836196ee8f12792384d6
dmulher <dominic.mulherin@gmail.com>
2023-12-17 22:24:11 +1000

    Improved with Dijkstra

--- solutions/src/day_17_a.rs
+++ solutions/src/day_17_a.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashMap, hash_map::DefaultHasher, VecDeque}, hash::{Hash, Hasher}};
+use std::{collections::{hash_map::DefaultHasher, BinaryHeap, HashSet}, hash::{Hash, Hasher}, cmp::{Ordering, Reverse}};
 
 extern crate test;
 
@@ -19,84 +19,122 @@ enum Direction {
   Right,
 }
 
-impl Direction {
-  fn travel(self, pos: (usize, usize), width: usize, height: usize) -> Option<(usize, usize)> {
-    match self {
-      Direction::Up => if pos.1 > 0 {Some((pos.0, pos.1 - 1))} else {None},
-      Direction::Down => if pos.1 < height - 1 {Some((pos.0, pos.1 + 1))} else {None},
-      Direction::Left => if pos.0 > 0 {Some((pos.0 - 1, pos.1))} else {None},
-      Direction::Right => if pos.0 < width - 1 {Some((pos.0 + 1, pos.1))} else {None},
-    }
-  }
+#[derive(PartialEq, Eq, Clone, Copy)]
+struct Node {
+  x: usize,
+  y: usize,
+  dir: Direction,
+  count: u8,
+  distance: u32,
+}
 
-  fn opposite(self) -> Direction {
-    match self {
-      Direction::Up => Direction::Down,
-      Direction::Down => Direction::Up,
-      Direction::Left => Direction::Right,
-      Direction::Right => Direction::Left,
+impl PartialOrd for Node {
+  fn ge(&self, other: &Self) -> bool {
+    return self.distance >= other.distance;
+  }
+  fn gt(&self, other: &Self) -> bool {
+    return self.distance > other.distance;
+  }
+  fn le(&self, other: &Self) -> bool {
+    return self.distance <= other.distance;
+  }
+  fn lt(&self, other: &Self) -> bool {
+    return self.distance < other.distance;
+  }
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    if self.distance > other.distance {
+      return Some(Ordering::Greater);
+    } else if self.distance == other.distance {
+      return Some(Ordering::Equal);
+    } else {
+      return Some(Ordering::Less);
     }
   }
 }
 
-fn hash(pos: &(usize, usize), dir: &Direction, count: &u8) -> u64 {
+impl Ord for Node {
+  fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+    if self.distance > other.distance {
+      return Ordering::Greater;
+    } else if self.distance == other.distance {
+      return Ordering::Equal;
+    } else {
+      return Ordering::Less;
+    }
+  }
+}
+
+impl Hash for Node {
+  fn hash<H: Hasher>(&self, state: &mut H) {
+    self.x.hash(state);
+    self.y.hash(state);
+    self.dir.hash(state);
+    self.count.hash(state);
+    self.distance.hash(state);
+  }
+}
+
+impl Node {
+  fn can_go_up(&self) -> bool {
+    self.y > 0 && ((self.dir == Direction::Up && self.count < 3) || self.dir == Direction::Left || self.dir == Direction::Right)
+  }
+  fn can_go_down(&self, height: usize) -> bool {
+    self.y < height - 1 && ((self.dir == Direction::Down && self.count < 3) || self.dir == Direction::Left || self.dir == Direction::Right)
+  }
+  fn can_go_left(&self) -> bool {
+    self.x > 0 && ((self.dir == Direction::Left && self.count < 3) || self.dir == Direction::Up || self.dir == Direction::Down)
+  }
+  fn can_go_right(&self, width: usize) -> bool {
+    self.x < width - 1 && ((self.dir == Direction::Right && self.count < 3) || self.dir == Direction::Up || self.dir == Direction::Down)
+  }
+
+  fn travel(&self, dir: &Direction, height: usize, width: usize) -> Option<(usize, usize)> {
+    match dir {
+      Direction::Up => if self.can_go_up() {Some((self.x, self.y - 1))} else {None},
+      Direction::Down => if self.can_go_down(height) {Some((self.x, self.y + 1))} else {None},
+      Direction::Left => if self.can_go_left() {Some((self.x - 1, self.y))} else {None},
+      Direction::Right => if self.can_go_right(width) {Some((self.x + 1, self.y))} else {None},
+    }
+  }
+}
+
+fn hash(x: usize, y: usize, dir: &Direction, count: u8) -> u64 {
   let mut h = DefaultHasher::new();
-  pos.hash(&mut h);
+  x.hash(&mut h);
+  y.hash(&mut h);
   dir.hash(&mut h);
   count.hash(&mut h);
   h.finish()
 }
 
 fn find_all_paths(heat_map: &Vec<Vec<u32>>) -> u32 {
-  let mut visited: HashMap<u64, u32> = HashMap::new();
-  let mut queue: VecDeque<(usize, usize, Direction, u8, u32)> = VecDeque::new();
+  let mut visited: HashSet<u64> = HashSet::new();
+  let mut distances: BinaryHeap<Reverse<Node>> = BinaryHeap::new();
   let height = heat_map.len();
   let width = heat_map[0].len();
-  let mut lowest_val: Option<u32> = None;
-  queue.push_back((0, 0, Direction::Right, 0, 0));
-  while let Some((x, y, direction, count, heat_val)) = queue.pop_front() {
-    let this_hash = hash(&(x, y), &direction, &count);
-    let this_heat = if x == 0 && y == 0 {0} else {heat_val + heat_map[x][y]};
-    let must_turn = count == 3;
 
-
-    if let Some(lv) = lowest_val {
-      if this_heat > lv {
-        continue;
-      }
-      if x == width - 1 && y == height - 1 && this_heat < lv {
-        lowest_val = Some(this_heat);
-        continue;
-      }
-    } else if x == width - 1 && y == height - 1 {
-      lowest_val = Some(this_heat);
-      continue;
+  distances.push(Reverse(Node{x: 0, y: 0, dir: Direction::Right, count: 0, distance: 0}));
+  while let Some(Reverse(node)) = distances.pop() {
+    if node.x == width - 1 && node.y == height - 1 {
+      return node.distance;
     }
-
-    if let Some(val) = visited.get(&this_hash) {
-      if val <= &this_heat {
-        continue;
-      }
-    }
-    visited.insert(this_hash, this_heat);
 
     [Direction::Up, Direction::Left, Direction::Down, Direction::Right]
       .into_iter()
       .filter_map(|dir| {
-        if dir.opposite() == direction {
-          return None;
-        }
-        if let Some(new_pos) = dir.travel((x, y), width, height) {
-          if direction != dir || !must_turn {
-            let new_dir_count = if direction == dir {count + 1} else {1};
-            return Some((new_pos.0, new_pos.1, dir, new_dir_count, this_heat));
+        if let Some((x, y)) = node.travel(&dir, width, height) {
+          let count = if node.dir == dir {node.count + 1} else {1};
+          if visited.insert(hash(x, y, &dir, count)) {
+            let distance = node.distance + heat_map[x][y];
+            let new_node = Node{x, y, dir, count, distance};
+            return Some(new_node);
           }
         }
         None
       })
-      .for_each(|new_check| queue.push_back(new_check));
+      .for_each(|new_node| distances.push(Reverse(new_node)));
   }
-  lowest_val.unwrap()
+  0
 }
 
 #[cfg(test)]
