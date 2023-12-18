10ee782ecc826522643221f70eb4fdadb40e1f4c
dmulher <dominic.mulherin@gmail.com>
2023-12-19 08:32:01 +1000

    Day 18

--- solutions/src/day_18_a.rs
+++ solutions/src/day_18_a.rs
@@ -1,9 +1,56 @@
-use std::collections::HashMap;
-
 extern crate test;
 
 pub fn main(contents: String) -> u32 {
-  0
+  dig_trench(contents)
+}
+
+fn dig_trench(contents: String) -> u32 {
+  let (_, total) = contents
+    .lines()
+    .map(|line| {
+      let mut split = line.split_whitespace();
+      let dir = Direction::from(split.next().unwrap());
+      let distance = split.next().unwrap().parse::<i32>().unwrap();
+      (dir, distance)
+    })
+    .fold(((0, 0), 0), |((x, y), total), (dir, distance)| {
+      let (change_x, change_y) = dir.to_change();
+      let (next_x, next_y) = (x + change_x * distance, y + change_y * distance);
+      ((next_x, next_y), total + (next_x + x) * (next_y - y) + distance)
+    });
+
+  (total as u32) / 2 + 1
+}
+
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+impl From<&str> for Direction {
+  fn from(value: &str) -> Self {
+    match value {
+      "R" => Direction::Right,
+      "L" => Direction::Left,
+      "U" => Direction::Up,
+      "D" => Direction::Down,
+      _ => panic!("Direction was wrong")
+    }
+  }
+}
+
+impl Direction {
+  fn to_change(&self) -> (i32, i32) {
+    match self {
+      Direction::Right => (1, 0),
+      Direction::Left => (-1, 0),
+      Direction::Up => (0, -1),
+      Direction::Down => (0, 1),
+    }
+  }
 }
 
 #[cfg(test)]
@@ -17,8 +64,8 @@ mod tests {
 
   #[test]
   fn test_day_18_a() {
-    const EXAMPLE_ANSWER: u32 = 0;
-    const ANSWER: Option<u32> = None;
+    const EXAMPLE_ANSWER: u32 = 62;
+    const ANSWER: Option<u32> = Some(48795);
     match utils::run_method::<u32>(&main, DAY, PART, (EXAMPLE_ANSWER, ANSWER)) {
       Err(message) => panic!("{}", message),
       Ok(val) if ANSWER.is_none() => println!("Answer for day {DAY}-{} = {val}", PART.lower_name()),
--- solutions/src/day_18_b.rs
+++ solutions/src/day_18_b.rs
@@ -1,9 +1,57 @@
-use std::collections::HashMap;
-
 extern crate test;
 
-pub fn main(contents: String) -> u32 {
-  0
+pub fn main(contents: String) -> u64 {
+  dig_trench(contents)
+}
+
+fn dig_trench(contents: String) -> u64 {
+  let (_, total) = contents
+    .lines()
+    .map(|line| {
+      let hex = line.split('#').skip(1).next().unwrap().replace(')', "");
+      let (distance_hex, dir_num) = hex.split_at(5);
+      let dir = Direction::from(dir_num);
+      let distance = i64::from_str_radix(distance_hex, 16).unwrap();
+      (dir, distance)
+    })
+    .fold(((0, 0), 0), |((x, y), total), (dir, distance)| {
+      let (change_x, change_y) = dir.to_change();
+      let (next_x, next_y) = (x + change_x * distance, y + change_y * distance);
+      ((next_x, next_y), total + (next_x + x) * (next_y - y) + distance)
+    });
+
+  (total as u64) / 2 + 1
+}
+
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+impl From<&str> for Direction {
+  fn from(value: &str) -> Self {
+    match value {
+      "0" => Direction::Right,
+      "1" => Direction::Down,
+      "2" => Direction::Left,
+      "3" => Direction::Up,
+      _ => panic!("Direction was wrong")
+    }
+  }
+}
+
+impl Direction {
+  fn to_change(&self) -> (i64, i64) {
+    match self {
+      Direction::Right => (1, 0),
+      Direction::Left => (-1, 0),
+      Direction::Up => (0, -1),
+      Direction::Down => (0, 1),
+    }
+  }
 }
 
 #[cfg(test)]
@@ -17,9 +65,9 @@ mod tests {
 
   #[test]
   fn test_day_18_b() {
-    const EXAMPLE_ANSWER: u32 = 0;
-    const ANSWER: Option<u32> = None;
-    match utils::run_method::<u32>(&main, DAY, PART, (EXAMPLE_ANSWER, ANSWER)) {
+    const EXAMPLE_ANSWER: u64 = 952408144115;
+    const ANSWER: Option<u64> = Some(40654918441248);
+    match utils::run_method::<u64>(&main, DAY, PART, (EXAMPLE_ANSWER, ANSWER)) {
       Err(message) => panic!("{}", message),
       Ok(val) if ANSWER.is_none() => println!("Answer for day {DAY}-{} = {val}", PART.lower_name()),
       _ => (),
